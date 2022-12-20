206c356866c7f9fe1477bd13366e446e9ad2b918
Ethan R <89108631+SFgiantsfan@users.noreply.github.com>
2022-12-19 20:44:36 -0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,30 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
+    // Scanner to see what game is being played
+    Scanner s1 = new Scanner(System.in);
 		Teams a = new StadiumTracker();
+    // Play boolean
 		boolean play = true;
-		if (play == true) {
-			System.out.println("Welcome");
+    //While loop to end app
+    while (play == true) {
+      //Introduction
+			System.out.println("Welcome to my Baseball App!\nThere are two apps involved: A Stadium Tracker and a Team Name Guesser.\nThe Stadium Tracker is where you can store information about the MLB stadiums you have been to. The Team Name Guesser is where you guess a MLB team based on information given.\nTo use the Stadium Tracker press 1 and to play the Team Name Guesser press 2!");
+      //What game?
+      int g = s1.nextInt();
+      if(g == 1){
+        //StadiumTracker();
+      } else if(g == 2) {
+        //TeamNameGuesser();
+      } else {
+        System.out.println("Invalid Imput.");
+        play = false;
+      }
+
+      play = false;
+      
 		}
 
 	}
