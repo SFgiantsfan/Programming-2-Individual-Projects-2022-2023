f16567a2263a0c2939371f67ae5da8faef8bb1bb
Ethan R <89108631+SFgiantsfan@users.noreply.github.com>
2022-12-21 08:50:39 -0700

    Update Teams.java

--- src/Teams.java
+++ src/Teams.java
@@ -1,5 +1,9 @@
 
 public class Teams {
 
-	String[][] team = {{"Braves", "National League", "East", "Hank Aaron", "Atlanta"}};
+	String[][] team = {{"Braves", "National League", "East", "Henry Aaron", "Atlanta", "Truist Part"}, 
+			{"Marlins", "National League", "East", "Miguel Cabrera (Early in carrer)", "Miami", "Loan Depot park"},
+			{"Mets", "National League", "East", "Tom Seaver", "New York", "Citi Field"},
+			{"Nationals", "National League", "East", "Ryan Zimmerman", "Atlanta", "Truist Part"},
+			{"Cubs", "National League", "Central", "Ernie Banks", "Chicago", "Wrigley Field"}};
 }
