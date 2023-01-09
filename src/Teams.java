663ea10114b61eaeb8bcc53e80911cd110e43f01
Ethan R <89108631+SFgiantsfan@users.noreply.github.com>
2023-01-09 08:48:58 -0700

    Update Teams.java

--- src/Teams.java
+++ src/Teams.java
@@ -5,5 +5,19 @@ public class Teams {
 			{"Marlins", "National League", "East", "Miguel Cabrera (Early in carrer)", "Miami", "Loan Depot park"},
 			{"Mets", "National League", "East", "Tom Seaver", "New York", "Citi Field"},
 			{"Nationals", "National League", "East", "Ryan Zimmerman", "Atlanta", "Truist Part"},
-			{"Cubs", "National League", "Central", "Ernie Banks", "Chicago", "Wrigley Field"}};
+			{"Cubs", "National League", "Central", "Ernie Banks", "Chicago", "Wrigley Field"},
+			{"Reds", "National League", "Central", "Pete Rose", "Cincinati", "Great American Ball Park"},
+			{"Brewers", "National League", "Central", "Christian Yelich", "Milwaukee", "American Family Field"},
+			{"Pirates", "National League", "Central", "Roberto Clemente", "Pittsburgh", "PNC Park"},
+			{"Cardinals", "National League", "Central", "Albert Pujols", "St. Louis", "Busch Stadium"},
+			{"Diamondbacks", "National League", "West", "Randy Johnson", "Arizona", "Chase Field"},
+			{"Rockies", "National League", "West", "Todd Helton", "Colorado", "Coors Field"},
+			{"Dodgers", "National League", "West", "Sandy Koufax", "Los Angeles", "Dodgers Stadium"},
+			{"Padres", "National League", "West", "Tony Gwynn", "San Diego", "Petco Park"},
+			{"Giants", "National League", "West", "Barry Bonds", "San Francisco", "Oracle Park"},
+			{"Orioles", "American League", "East", "Cal Ripken Jr.", "Baltimore", "Camden Yards"},
+			{"Red Sox", "American League", "East", "Ted Williams", "Boston", "Fenway Park"},
+			{"Yankees", "American League", "East", "Derek Jeter", "New York", "Yankee Stadium"},
+			{"Rays", "American League", "East", "Evan Longoria", "Tampa Bay", "Tropicana Field"},
+			{"Blue Jays", "American League", "East", "Roberto Alomar", "Toronto", "Rogers Centre"}};
 }
