75ca929d36b127808743f2dc091c55d60456d8c8
Ethan R <89108631+SFgiantsfan@users.noreply.github.com>
2023-02-13 08:54:58 -0700

    Update Teams.java

--- src/Teams.java
+++ src/Teams.java
@@ -1,23 +1,128 @@
 
 public class Teams {
+	
+	Teams(){
+	String[][] team = new String[32][6];
+	
+	// NL East
+	// Atlanta Braves
+	team [0][0] = "Braves";
+	team [0][1] = "National League";
+	team [0][2] = "East";
+	team [0][3] = "Henry Aaron";
+	team [0][4] = "Truist Part";
+	team [0][5] = "Atlanta";
+	
+	//Miami Marlins
+	team [1][0] = "Marlins";
+	team [1][1] = "National League";
+	team [1][2] = "East";
+	team [1][3] = "Miguel Cabrera";
+	team [1][4] = "Loan Depot park";
+	team [1][5] = "Miami";
+	
+	// New York Mets
+	team [2][0] = "Mets";
+	team [2][1] = "National League";
+	team [2][2] = "East";
+	team [2][3] = "Tom Seaver";
+	team [2][4] = "Citi Field";
+	team [2][5] = "New York";
+	
+	// Washington Nationals
+	team [3][0] = "Nationals";
+	team [3][1] = "National League";
+	team [3][2] = "East";
+	team [3][3] = "Ryan Zimmerman";
+	team [3][4] = "Truist Part";
+	team [3][5] = "Washington";
 
-	String[][] team = {{"Braves", "National League", "East", "Henry Aaron", "Atlanta", "Truist Part"}, 
-			{"Marlins", "National League", "East", "Miguel Cabrera (Early in carrer)", "Miami", "Loan Depot park"},
-			{"Mets", "National League", "East", "Tom Seaver", "New York", "Citi Field"},
-			{"Nationals", "National League", "East", "Ryan Zimmerman", "Atlanta", "Truist Part"},
-			{"Cubs", "National League", "Central", "Ernie Banks", "Chicago", "Wrigley Field"},
-			{"Reds", "National League", "Central", "Pete Rose", "Cincinati", "Great American Ball Park"},
-			{"Brewers", "National League", "Central", "Christian Yelich", "Milwaukee", "American Family Field"},
-			{"Pirates", "National League", "Central", "Roberto Clemente", "Pittsburgh", "PNC Park"},
-			{"Cardinals", "National League", "Central", "Albert Pujols", "St. Louis", "Busch Stadium"},
-			{"Diamondbacks", "National League", "West", "Randy Johnson", "Arizona", "Chase Field"},
-			{"Rockies", "National League", "West", "Todd Helton", "Colorado", "Coors Field"},
-			{"Dodgers", "National League", "West", "Sandy Koufax", "Los Angeles", "Dodgers Stadium"},
-			{"Padres", "National League", "West", "Tony Gwynn", "San Diego", "Petco Park"},
-			{"Giants", "National League", "West", "Barry Bonds", "San Francisco", "Oracle Park"},
-			{"Orioles", "American League", "East", "Cal Ripken Jr.", "Baltimore", "Camden Yards"},
-			{"Red Sox", "American League", "East", "Ted Williams", "Boston", "Fenway Park"},
-			{"Yankees", "American League", "East", "Derek Jeter", "New York", "Yankee Stadium"},
-			{"Rays", "American League", "East", "Evan Longoria", "Tampa Bay", "Tropicana Field"},
-			{"Blue Jays", "American League", "East", "Roberto Alomar", "Toronto", "Rogers Centre"}};
+	
+	// NL Central
+	// Chicago Cubs
+	team [4][0] = "Cubs";
+	team [4][1] = "National League";
+	team [4][2] = "Central";
+	team [4][3] = "Ernie Banks";
+	team [4][4] = "Wrigley Field";
+	team [4][5] = "Chicago";
+	
+	// Cincinati Reds
+	team [5][0] = "Reds";
+	team [5][1] = "National League";
+	team [5][2] = "Central";
+	team [5][3] = "Pete Rose";
+	team [5][4] = "Great American Ball Park";
+	team [5][5] = "Cincinati";
+	
+	// Milwaukee Brewers
+	team [6][0] = "Brewers";
+	team [6][1] = "National League";
+	team [6][2] = "Central";
+	team [6][3] = "Christian Yelich";
+	team [6][4] = "American Family Field";
+	team [6][5] = "Milwaukee";
+	
+	// Pittsburgh Pirates
+	team [7][0] = "Pirates";
+	team [7][1] = "National League";
+	team [7][2] = "Central";
+	team [7][3] = "Roberto Clemente";
+	team [7][4] = "PNC Park";
+	team [7][5] = "Pittsburgh";
+	
+	// St. Louis Cardinals
+	team [8][0] = "Cardinals";
+	team [8][1] = "National League";
+	team [8][2] = "Central";
+	team [8][3] = "Albert Pujols";
+	team [8][4] = "Busch Stadium";
+	team [8][5] = "St. Louis";
+	
+	// NL West
+	// Arizona Dimondbacks
+	team [9][0] = "Diamondbacks";
+	team [9][1] = "National League";
+	team [9][2] = "West";
+	team [9][3] = "Randy Johnson";
+	team [9][4] = "Chase Field";
+	team [9][5] = "Arizona";
+	
+	// Colorado Rockies
+	team [10][0] = "Rockies";
+	team [10][1] = "National League";
+	team [10][2] = "West";
+	team [10][3] = "Todd Helton";
+	team [10][4] = "Coors Field";
+	team [10][5] = "Colorado";
+	
+	// LA Dodgers
+	team [11][0] = "Dodgers";
+	team [11][1] = "National League";
+	team [11][2] = "West";
+	team [11][3] = "Sandy Koufax";
+	team [11][4] = "Dodgers Stadium";
+	team [11][5] = "Los Angeles";
+	
+	// San Diego Padres
+	team [12][0] = "Padres";
+	team [12][1] = "National League";
+	team [12][2] = "West";
+	team [12][3] = "Tony Gwynn";
+	team [12][4] = "Petco Park";
+	team [12][5] = "San Diego";
+	
+	}
 }
+
+
+
+
+
+//	{"Giants", "National League", "West", "Barry Bonds", "San Francisco", "Oracle Park"},
+//	
+//	{"Orioles", "American League", "East", "Cal Ripken Jr.", "Baltimore", "Camden Yards"},
+//	{"Red Sox", "American League", "East", "Ted Williams", "Boston", "Fenway Park"},
+//	{"Yankees", "American League", "East", "Derek Jeter", "New York", "Yankee Stadium"},
+//	{"Rays", "American League", "East", "Evan Longoria", "Tampa Bay", "Tropicana Field"},
+//	{"Blue Jays", "American League", "East", "Roberto Alomar", "Toronto", "Rogers Centre"}
