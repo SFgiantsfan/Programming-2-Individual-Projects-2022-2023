4a5277d50cab0163dc6f4fc99383f776a4fc2e66
Ethan R <89108631+SFgiantsfan@users.noreply.github.com>
2023-02-22 09:03:33 -0700

    Create StadiumTracker.java

---
+++ src/Java/StadiumTracker.java
@@ -0,0 +1,20 @@
+
+public class StadiumTracker extends Teams {
+	private int rating;
+	private String memEvents;
+	private String notes;
+
+	public StadiumTracker() {
+		System.out.println("Test");
+	}
+
+	/*String team() {
+		if(team[0][1] == "Atlanta") {
+			return "Hi";
+		} else {
+			return "no";
+		}
+		
+	}*/
+
+}
